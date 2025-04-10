502214cef8966deefd710f34fd461d76eac29cd0
leaviki90 <leaviki90@gmail.com>
2025-04-10 17:37:26 +0200

    end to end test finished

--- tests/UIBasicstest.spec.js
+++ tests/UIBasicstest.spec.js
@@ -81,7 +81,7 @@ test('UI Controls', async ({ page }) => {
 })
 
 
-test.only('Child windows handling', async ({ browser}) => {
+test('Child windows handling', async ({ browser}) => {
     const context = await browser.newContext();
     const page = await context.newPage()
     await page.goto('https://rahulshettyacademy.com/loginpagePractise/')
